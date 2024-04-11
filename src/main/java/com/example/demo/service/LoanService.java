e209d25f9c9a63eda6bc70b29a6f26952aa413e2
Valery <valeryroj@yandex.ru>
2024-04-11 15:33:03 +0300

    working with starter

--- src/main/java/com/example/demo/service/LoanService.java
+++ src/main/java/com/example/demo/service/LoanService.java
@@ -2,19 +2,21 @@ package com.example.demo.service;
 
 import com.example.demo.property.LoanProperties;
 import com.example.demo.repository.CarRepository;
+
+import com.starter.userincomestarter.IncomeClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
 
 @Service
 @AllArgsConstructor
 public class LoanService {
 
-    private final UserIncomeService userIncomeService;
+    private final IncomeClient incomeClient;
     private final CarRepository carRepository;
     private final LoanProperties loanProperties;
+
     public int approveLoan(int userId) {
-        int userIncome = userIncomeService.getUserIncome(userId);
+        int userIncome = incomeClient.getUserIncome(userId);
         int divisor = loanProperties.getUserIncomeDivisor();
         int minIncome = loanProperties.getMinIncome();
         int minimalCostOfCar = loanProperties.getMinimalCostOfCar();
